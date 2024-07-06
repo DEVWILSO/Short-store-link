c01f46360d0792eacdbd6190c6fcb9d92be753de
DevWilson <106603032+DEVWILSO@users.noreply.github.com>
2024-07-06 10:29:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,7 +67,7 @@ SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "269bd24b7ea6d49e1307b13
 #add your custom time in secs for shortlink expiration.
 # 24hr = 86400
 # 12hr = 43200
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "43200")) # Add time in seconds
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "120")) # Add time in seconds
 #Tutorial video for the user of your shortner on how to download.
 TUT_VID = os.environ.get("TUT_VID","https://t.me/Demoshorts/41")
 
