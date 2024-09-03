699dc1553f4859ac0314cb4dde5f77d3cdd79396
DevWilson <106603032+DEVWILSO@users.noreply.github.com>
2024-09-03 19:27:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -60,10 +60,10 @@ https://dashboard.shareus.io/
 # TRUE for yes FALSE if no 
 USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "TRUE") == "TRUE" else False 
 # only shareus service known rightnow rest you can test on your own
-SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "instantearn.in")
-# SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "f9935abcdc64aaf45eda33edaa8d4e76259a10b1")
+SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "tnshort.net")
+# SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "0e8a86b9c1c70a8ee2593d491983f077446b9695")
 #use this key if not working ☠️ (jokin!!)
-SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "f9935abcdc64aaf45eda33edaa8d4e76259a10b1")
+SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "0e8a86b9c1c70a8ee2593d491983f077446b9695")
 #add your custom time in secs for shortlink expiration.
 # 24hr = 86400
 # 12hr = 43200
